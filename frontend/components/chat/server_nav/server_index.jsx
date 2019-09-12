c915de35a394353c4a95f446289134f2731ea5e3
Ash Todd <ashztodd@gmail.com>
2019-09-12 16:59:01 -0700

    Made popup modal pop up

--- frontend/components/chat/server_nav/server_index.jsx
+++ frontend/components/chat/server_nav/server_index.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import ServerIndexItem from './server_index_item';
+import PickServerForm from './server_modal/pick_server_form';
 
 export default class ServerIndex extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            
+            showForm: false
         }
+        this.showForm = this.showForm.bind(this);
+        this.hideForm = this.hideForm.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchServers();
     }
 
-    handleNewServer() {
+    showForm() {
+        this.setState({
+            showForm: true
+        })
+    }
 
+    hideForm() {
+        this.setState({
+            showForm: false
+        })
     }
 
     render() {
@@ -28,11 +39,11 @@ export default class ServerIndex extends React.Component {
                     {this.props.servers.map((server) => {
                         return <ServerIndexItem server={server} key={server.id}/>
                     })}
-                    <li><button className="new-server-button tooltip"><span className="server-placeholder">+</span>
+                    <li><button className="new-server-button tooltip" onClick={this.showForm}><span className="server-placeholder">+</span>
                     <span className="tooltip-text">Add Server</span></button>
                     </li>
                 </ul>
-                
+                {this.state.showForm ? <PickServerForm hideForm={this.hideForm}/> : ""}
             </div>
         )
     }
--- frontend/components/chat/server_nav/server_modal/pick_server_form.jsx
+++ frontend/components/chat/server_nav/server_modal/pick_server_form.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
 
-export default class pickServerForm extends React.Component {
+import NewServerFormContainer from './new_server_form_container';
+import JoinServerFormContainer from './join_server_form_container';
+
+export default class PickServerForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            formType: "pick"
+            formType: ""
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(type) {
+        return (e) => {
+            e.preventDefault();
+            this.setState({formType: type});
         }
     }
 
+
     render() {
 
+        let form;
 
-        <div className="server-form-backdrop">
-            {}
-        </div>
+        switch (this.state.formType) {
+            case "new":
+                form = <NewServerFormContainer />;
+                break;
+            case "join":
+                form = <JoinServerFormContainer />;
+                break;
+            default:
+                form = (<div className="pick-server-form-container">
+                    <h2>Pick your source</h2>
+                    <div>
+                        <button onClick={this.handleClick("new")}>Create a server</button>
+                        <button onClick={this.handleClick("join")}>Join a server</button>
+                    </div>
+                </div>);
+                break;
+        };
+
+        console.log(form);
+
+        return (
+            <div className="server-form">
+                {form}
+                <div className="server-form-backdrop" onClick={this.props.hideForm}></div>
+            </div>
+        )
     }
 }
