f32723e31a5eb12e9c1097318455cf14cc62aea4
Ash Todd <ashztodd@gmail.com>
2019-09-20 10:47:33 -0700

    Messages look slightly less that way

--- frontend/components/chat/message_area/message/message.jsx
+++ frontend/components/chat/message_area/message/message.jsx
@@ -4,10 +4,10 @@ class Message extends React.Component {
 
     render() {
         return (
-            <li key = { this.props.message.id }>
+            <li key = { this.props.message.id } className="message">
                 <div>
                     <span className="nick">{this.props.message.nick || this.props.username}</span>
-                    <span className="time-sent">{this.props.message.created_at}</span>
+                    <span className="time-sent">{this.props.message.created_at.match(/\d\d:\d\d/)}</span>
                 </div>
                 <span className="message-body">{this.props.message.body}</span>
             </li>
